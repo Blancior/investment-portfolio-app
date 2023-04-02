7cd63998ea7b4f7014ba46cedd1020d3caf4d1f3
blancior <pkedziora347@gmail.com>
2023-04-02 21:08:39 +0200

    Current price indicator

--- src/app/dashboard/asset/asset.component.ts
+++ src/app/dashboard/asset/asset.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {DashboardComponent} from "../dashboard.component";
+import axios from "axios";
 
 @Component({
 selector: 'asset',
@@ -9,16 +10,26 @@ styleUrls: ['./asset.component.scss']
 })
 export class AssetComponent implements OnInit{
   apikey= '4697a2f8-01cf-4bd3-9d0b-29f3a4d1b835';
-  selectedCrypto = '';
+  selectedCrypto;
   cryptoPrice :any;
-  searchForm : FormGroup;
   ngOnInit() {
-      this.searchForm = new FormGroup({
-        assetName: new FormControl(null)
-      });
     }
-  selectedCrypto1;
-  onSubmit(){
-    console.log(this.searchForm.value.assetName);
-  }
+    constructor(public dashboard: DashboardComponent) {
+    }
+  selectedCryptoPrice;
+  onSubmit = async () => {
+       axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+         params: {
+           vs_currency: 'usd',
+           ids: this.selectedCrypto.toString()
+         }
+       }).then(response => {
+           console.log(response.data[0].current_price);
+           this.selectedCryptoPrice=response.data[0].current_price;
+           document.getElementById("ind1").style.visibility='visible';
+         }).catch(error => {
+           console.error(error);
+         });
+     }
+
 }
