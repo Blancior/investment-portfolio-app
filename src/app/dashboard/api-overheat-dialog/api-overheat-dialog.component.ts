7b5a18f59bda3a78ae7b2630fb3753d5c33b506a
blancior <pkedziora347@gmail.com>
2023-11-19 14:59:04 +0100

    Feature: added info about API overheat

---
+++ src/app/dashboard/api-overheat-dialog/api-overheat-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-overheat-dialog',
+  templateUrl: './api-overheat-dialog.component.html',
+  styleUrls: ['./api-overheat-dialog.component.scss']
+})
+export class ApiOverheatDialogComponent {
+
+}
--- src/app/dashboard/asset/asset.component.ts
+++ src/app/dashboard/asset/asset.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import axios from "axios";
+import {MatDialog} from "@angular/material/dialog";
+import {ApiOverheatDialogComponent} from "../api-overheat-dialog/api-overheat-dialog.component";
 
 @Component({
 selector: 'asset',
@@ -8,9 +10,10 @@ styleUrls: ['./asset.component.scss']
 
 })
 export class AssetComponent{
-  selectedCrypto:string;
-  displayedRN:string='bitcoin';
-  cryptoPrice :any;
+  selectedCrypto:string='';
+  displayedRN:string = 'bitcoin';
+  cryptoPrice: any;
+  coinImage!:string;
   date: any;
   coinNames1: string[] = [
     'bitcoin', 'ethereum', 'ripple', 'dogecoin', 'cardano', 'binancecoin', 'solana', 'polkadot', 'litecoin', 'tron', 'shiba-inu', 'dai', 'uniswap', 'chainlink', 'wrapped-bitcoin',
@@ -19,22 +22,36 @@ export class AssetComponent{
     'dash', 'cake', 'iota', 'gmx', 'zilliqa', '1inch', 'osmosis', 'floki', 'dydx', 'woo-network', 'link', 'gala', 'lisk'
   ]; //temp
   selectedCryptoPrice=0;
-  onSubmit = () => {
-       axios.get('https://api.coingecko.com/api/v3/coins/markets', {
-         params: {
-           vs_currency: 'usd',
-           ids: this.selectedCrypto
-         }
-       }).then(response => {
-           this.selectedCryptoPrice=response.data[0].current_price;
-           if (this.selectedCryptoPrice.toString().length>5)
-             Math.round(this.selectedCryptoPrice);
-           this.displayedRN=this.selectedCrypto.toUpperCase();
-           document.getElementById("ind2").style.visibility='visible';
-           this.date = new Date();
-         }).catch(error => {
-           console.error(error);
-         });
-     }
 
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  onSubmit = () => {
+         if (this.selectedCrypto!= ''){
+           axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+             params: {
+               vs_currency: 'usd',
+               ids: this.selectedCrypto
+             }
+           }).then(response => {
+             console.log(response.data)
+             this.selectedCryptoPrice=response.data[0].current_price;
+             if (this.selectedCryptoPrice.toString().length>5)
+               Math.round(this.selectedCryptoPrice*100)/100;
+             this.displayedRN=response.data[0].name;
+             this.coinImage = response.data[0].image;
+             document.getElementById("ind2").style.visibility='visible';
+             this.date = new Date();
+           }).catch(error => {
+             this.dialog.open(ApiOverheatDialogComponent,{
+               disableClose: false
+             })
+             console.log(error);
+           });
+         }
+     }
+fun(){
+  console.log(this.selectedCrypto)
+}
 }
